f8ebb069694b55399ed47a8e2c9efdebb41ec3c5
CarlosMPerez <carlosm.perez@gmail.com>
2025-06-03 00:07:27 +0200

    Minor fix

--- Movies.Contracts/Requests/UpdateMovieRequest.cs
+++ Movies.Contracts/Requests/UpdateMovieRequest.cs
@@ -3,7 +3,6 @@ using System;
 namespace Movies.Contracts.Requests;
 public class UpdateMovieRequest
 {
-    public required Guid Id { get; init; }
     public required string Title { get; init; }
     public required int ReleaseYear { get; init; }
     public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
